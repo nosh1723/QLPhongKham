becfb29ca9ab226f7fe841bb0285e3f61ad3745f
luongnhan <luongkute2k3@gmail.com>
2024-06-11 13:15:48 +0700

    dlick

--- client/app/MakeAppointment/_layout.tsx
+++ client/app/MakeAppointment/_layout.tsx
@@ -41,39 +41,39 @@ export default function Makeappointment() {
         <GestureHandlerRootView >
             <>
                 <ScrollView showsVerticalScrollIndicator={false} showsHorizontalScrollIndicator={false}>
-                    <View style={{ flexDirection: 'row', padding: 10, backgroundColor: '#fff', marginBottom: 10 }}>
-                        <Text style={{ height: 20, width: 20, borderRadius: 20, textAlign: 'center', backgroundColor: '#696969', color: "#fff" }}>1</Text>
-                        <Text style={{ marginLeft: 5, fontSize: 13 }}>Chọn lịch khám</Text>
-                        <AntDesign name="right" size={16} color="black" style={{ marginLeft: 12, marginRight: 12 }} />
-                        <Text style={{ height: 20, width: 20, borderRadius: 20, textAlign: 'center', backgroundColor: '#696969', color: "#fff" }}>2</Text>
-                        <Text style={{ marginLeft: 5, fontSize: 13 }}>Xác nhận</Text>
-                        <AntDesign name="right" size={16} color="black" style={{ marginLeft: 12, marginRight: 12 }} />
-                        <Text style={{ height: 20, width: 20, borderRadius: 20, textAlign: 'center', backgroundColor: '#696969', color: "#fff" }}>3</Text>
-                        <Text style={{ marginLeft: 5, fontSize: 13 }}>Nhận lịch hẹn</Text>
-                    </View>
-                    <View style={{ flexDirection: "row", padding: 12, backgroundColor: '#fff', marginBottom: 5 }}>
-                        <Image containerStyle={{ width: 100, height: 100, borderRadius: 1000, marginBottom: 10, marginRight: 15 }} source={{ uri: "https://i.pinimg.com/736x/7d/9d/ed/7d9ded7751b328b1000bcfe4c1dc7727.jpg" }} />
-                        <View>
-                            <Text style={{ fontSize: 18 }}>BS. CK2</Text>
-                            <Text style={{ fontSize: 20, fontWeight: 500 }}>
-                                Lê Thị Minh Hồng
-                            </Text>
-                            <View style={{ flexDirection: 'row', alignItems: "center", paddingVertical: 5 }}>
-                                <MaterialCommunityIcons name="check-decagram" size={16} color="#007bfc" style={{ paddingRight: 3 }} />
-                                <Text style={{ color: "#007bfc" }}>Bác sĩ</Text>
-                            </View>
-                            <Text><Text style={{ fontWeight: 500 }}>24</Text> năm kinh nghiệm</Text>
-                            <Text numberOfLines={1}>Cơ sở 1: Đường tam trinh, phố đèn xanh</Text>
+                    <View style={{ backgroundColor: '#f0f5fa' }}>
+                        <View style={{ flexDirection: 'row', padding: 10, backgroundColor: '#fff', marginBottom: 10 }}>
+                            <Text style={{ height: 20, width: 20, borderRadius: 20, textAlign: 'center', backgroundColor: '#696969', color: "#fff" }}>1</Text>
+                            <Text style={{ marginLeft: 5, fontSize: 13 }}>Chọn lịch khám</Text>
+                            <AntDesign name="right" size={16} color="black" style={{ marginLeft: 12, marginRight: 12 }} />
+                            <Text style={{ height: 20, width: 20, borderRadius: 20, textAlign: 'center', backgroundColor: '#696969', color: "#fff" }}>2</Text>
+                            <Text style={{ marginLeft: 5, fontSize: 13 }}>Xác nhận</Text>
+                            <AntDesign name="right" size={16} color="black" style={{ marginLeft: 12, marginRight: 12 }} />
+                            <Text style={{ height: 20, width: 20, borderRadius: 20, textAlign: 'center', backgroundColor: '#696969', color: "#fff" }}>3</Text>
+                            <Text style={{ marginLeft: 5, fontSize: 13 }}>Nhận lịch hẹn</Text>
                         </View>
-                    </View>
-                    <View style={styles.container}>
-                        <View>
+                        <View style={{ flexDirection: "row", padding: 12, backgroundColor: '#fff', marginBottom: 5 }}>
+                            <Image containerStyle={{ width: 100, height: 100, borderRadius: 1000, marginBottom: 10, marginRight: 15 }} source={{ uri: "https://i.pinimg.com/736x/7d/9d/ed/7d9ded7751b328b1000bcfe4c1dc7727.jpg" }} />
+                            <View>
+                                <Text style={{ fontSize: 18 }}>BS. CK2</Text>
+                                <Text style={{ fontSize: 20, fontWeight: 500 }}>
+                                    Lê Thị Minh Hồng
+                                </Text>
+                                <View style={{ flexDirection: 'row', alignItems: "center", paddingVertical: 5 }}>
+                                    <MaterialCommunityIcons name="check-decagram" size={16} color="#007bfc" style={{ paddingRight: 3 }} />
+                                    <Text style={{ color: "#007bfc" }}>Bác sĩ</Text>
+                                </View>
+                                <Text><Text style={{ fontWeight: 500 }}>24</Text> năm kinh nghiệm</Text>
+                                <Text numberOfLines={1}>Cơ sở 1: Đường tam trinh, phố đèn xanh</Text>
+                            </View>
+                        </View>
+                        <View style={styles.container}>
                             <View>
                                 <View>
                                     <Text style={{ fontWeight: '500', fontSize: 16, }}>Đặt lịch khám này cho:</Text>
                                 </View>
-                                < View style={{}}>
-                                    <View style={{ marginTop: 10, borderRadius: 10, padding: 12, backgroundColor: '#ffffff' }}>
+                                < View style={{ marginTop: 10, backgroundColor: '#ffffff', borderRadius: 15, }}>
+                                    <View style={{ padding: 12, }}>
                                         <View style={styles.textrow}>
                                             <Text >Họ và tên</Text>
                                             <Text style={styles.textcell}>Nguyễn Khắc Lương</Text>
@@ -91,66 +91,66 @@ export default function Makeappointment() {
                                             <Text style={styles.textcell}>0898584206</Text>
                                         </View>
                                     </View>
-                                    <View style={{ flexDirection: 'row', backgroundColor: '#DCDCDC', height: 43, width: '100%', borderBottomLeftRadius: 15, borderBottomRightRadius: 15, padding: 15 }}>
-                                        <Text style={{ marginRight: '60%', lineHeight: 15, fontWeight: '400' }}>Xem chi tiết</Text>
-                                        <Text style={{ lineHeight: 15, color: 'blue', }}>Sửa hồ sơ</Text>
+                                    <View style={{ flexDirection: 'row', backgroundColor: '#e1e8f2', height: 46, padding: 15, justifyContent: 'space-between', borderBottomLeftRadius: 15, borderBottomRightRadius: 15 }}>
+                                        <Text style={{ textAlign: 'center', fontWeight: '400' }}>Xem chi tiết</Text>
+                                        <Text style={{ color: 'blue', textAlign: 'center', }}>Sửa hồ sơ</Text>
                                     </View>
                                 </View>
-                            </View>
-                            <View>
-                                <Text style={{ fontWeight: '500', fontSize: 16, marginTop: 10 }}>Chọn Ngày khám</Text>
-                                <CommonButton onPress={() => {
-                                    show ? bottomSheetRef.current?.close() : bottomSheetRef.current?.expand()
-                                    setShow(!show)
-                                }} style={{ borderWidth: 1, borderColor: "#e7ebed", borderRadius: 8 }} color='transparent'>
-                                    <View style={{ flexDirection: 'row', alignItems: "center" }}>
-                                        <Text style={{ fontWeight: 600 }}>{getDateFormat(date)} </Text>
-                                        <FontAwesome name="angle-down" size={20} color="black" style={{ paddingLeft: 3, marginTop: -3 }} />
+                                <View>
+                                    <Text style={{ fontWeight: '500', fontSize: 16, marginTop: 10, marginBottom: 10 }}>Chọn Ngày khám</Text>
+                                    <CommonButton onPress={() => {
+                                        show ? bottomSheetRef.current?.close() : bottomSheetRef.current?.expand()
+                                        setShow(!show)
+                                    }} style={{ borderWidth: 1, borderColor: "#e7ebed", borderRadius: 8 }} color='transparent'>
+                                        <View style={{ flexDirection: 'row', alignItems: "center" }}>
+                                            <Text style={{ fontWeight: 600 }}>{getDateFormat(date)} </Text>
+                                            <FontAwesome name="angle-down" size={20} color="black" style={{ paddingLeft: 3, marginTop: -3 }} />
+                                        </View>
+                                    </CommonButton>
+                                    {!isIos && show && <DateTimePicker
+                                        testID="dateTimePicker"
+                                        value={date}
+                                        mode={mode}
+                                        is24Hour={true}
+                                        onChange={onChange}
+                                        display='spinner'
+                                        style={{ height: 120 }}
+                                    />}
+                                </View>
+                                <Text style={{ fontWeight: '500', fontSize: 16, marginTop: 10 }}>Chọn giờ khám</Text>
+                                <View style={styles.table}>
+                                    {/* Header */}
+                                    <View style={styles.row}>
+                                        <Text style={styles.cellHeader}>Buổi sáng</Text>
+                                        <Text style={styles.cellHeader}>Buổi chiều</Text>
                                     </View>
-                                </CommonButton>
-                                {!isIos && show && <DateTimePicker
-                                    testID="dateTimePicker"
-                                    value={date}
-                                    mode={mode}
-                                    is24Hour={true}
-                                    onChange={onChange}
-                                    display='spinner'
-                                    style={{ height: 120 }}
-                                />}
-                            </View>
-                            <Text style={{ fontWeight: '500', fontSize: 16, marginTop: 10 }}>Chọn giờ khám</Text>
-                            <View style={styles.table}>
-                                {/* Header */}
-                                <View style={styles.row}>
-                                    <Text style={styles.cellHeader}>Buổi sáng</Text>
-                                    <Text style={styles.cellHeader}>Buổi chiều</Text>
+                                    {/* Rows */}
+                                    <View style={styles.row}>
+                                        <Text style={styles.cell}>17:30 - 17:40</Text>
+                                        <Text style={styles.cell}>17:40 - 17:50</Text>
+                                        <Text style={styles.cell}>17:50 - 18:00</Text>
+                                    </View>
+                                    <View style={styles.row}>
+                                        <Text style={styles.cell}>18:00 - 18:10</Text>
+                                        <Text style={styles.cell}>18:10 - 18:20</Text>
+                                        <Text style={styles.cell}>18:20 - 18:30</Text>
+                                    </View>
+                                    <View style={styles.row}>
+                                        <Text style={styles.cell}>18:30 - 18:40</Text>
+                                        <Text style={styles.cell}>18:40 - 18:50</Text>
+                                        <Text style={styles.cell}>18:50 - 19:00</Text>
+                                    </View>
+                                    <View style={styles.row}>
+                                        <Text style={styles.cell}>19:00 - 19:10</Text>
+                                        <Text style={styles.cell}>19:10 - 19:20</Text>
+                                        <Text style={styles.cell}>19:20 - 19:30</Text>
+                                    </View>
                                 </View>
-                                {/* Rows */}
-                                <View style={styles.row}>
-                                    <Text style={styles.cell}>17:30 - 17:40</Text>
-                                    <Text style={styles.cell}>17:40 - 17:50</Text>
-                                    <Text style={styles.cell}>17:50 - 18:00</Text>
+                                <View>
+                                    <Text style={{ fontWeight: '500', fontSize: 16, marginTop: 30 }}>Thông tin bổ sung (Không bắt buộc)</Text>
+                                    <Text style={{ marginTop: 8 }}>Bạn có thể cung cấp thêm các thông tin như lý do khám, triệu chứng, đơn thuốc sử dụng gần đây</Text>
+                                    <Text style={{ color: 'blue', height: 40, backgroundColor: '#ffffff', lineHeight: 40, textAlign: 'center', marginTop: 15, borderRadius: 10, }}>Tôi muốn gửi thêm thông tin </Text>
                                 </View>
-                                <View style={styles.row}>
-                                    <Text style={styles.cell}>18:00 - 18:10</Text>
-                                    <Text style={styles.cell}>18:10 - 18:20</Text>
-                                    <Text style={styles.cell}>18:20 - 18:30</Text>
-                                </View>
-                                <View style={styles.row}>
-                                    <Text style={styles.cell}>18:30 - 18:40</Text>
-                                    <Text style={styles.cell}>18:40 - 18:50</Text>
-                                    <Text style={styles.cell}>18:50 - 19:00</Text>
-                                </View>
-                                <View style={styles.row}>
-                                    <Text style={styles.cell}>19:00 - 19:10</Text>
-                                    <Text style={styles.cell}>19:10 - 19:20</Text>
-                                    <Text style={styles.cell}>19:20 - 19:30</Text>
-                                </View>
-                            </View>
-                            <View>
-                                <Text style={{ fontWeight: '500', fontSize: 16, marginTop: 30 }}>Thông tin bổ sung (Không bắt buộc)</Text>
-                                <Text style={{ marginTop: 8 }}>Bạn có thể cung cấp thêm các thông tin như lý do khám, triệu chứng, đơn thuốc sử dụng gần đây</Text>
-                                <Text style={{ color: 'blue', height: 40, backgroundColor: '#ffffff', lineHeight: 40, textAlign: 'center', marginTop: 15, borderRadius: 10, }}>Tôi muốn gửi thêm thông tin </Text>
                             </View>
                         </View>
                     </View>
